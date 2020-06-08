e660c83b00cce3a9922ffd6435e07bca7917089a
Izabela Gomes <igomes@pivotal.io>
2020-06-08 17:25:24 -0400

    Moved provider to dashboard module
    Signed-off-by: Izabela Gomes <igomes@pivotal.io>

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 #################################
-# PROVIDERS
-#################################
-
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-}
-
-#################################
 # MODULES
 #################################
 
