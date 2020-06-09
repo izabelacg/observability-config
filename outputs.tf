65ed1dcd9f8441a3cf0df08dcc56d33b1d8b4be3
Izabela Gomes <igomes@pivotal.io>
2020-06-09 18:16:24 -0400

    Module outputs dashboards' urls
    Signed-off-by: Izabela Gomes <igomes@pivotal.io>

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,27 @@
 #################################
 # OUTPUT
 #################################
+
+output "ci_dashboard_url" {
+  value       = module.ci_dashboard.url
+}
+
+output "ci_dashboard_system_stats_url" {
+  value       = module.ci_dashboard.system_stats_url
+}
+
+output "ci_bosh_dashboard_url" {
+  value       = module.ci_bosh_dashboard.url
+}
+
+output "ci_bosh_dashboard_system_stats_url" {
+  value       = module.ci_bosh_dashboard.system_stats_url
+}
+
+output "hh_dashboard_url" {
+  value       = module.hush_house_dashboard.url
+}
+
+output "hh_dashboard_system_stats_url" {
+  value       = module.hush_house_dashboard.system_stats_url
+}
